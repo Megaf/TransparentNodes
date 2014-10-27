b4ecae0309312b474081dc0197a271a805282352
Megaf <mmegaf@gmail.com>
2014-10-27 19:44:39 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,18 @@
-TransparentNodes = {}
+transparent_nodes = {}
 
 minetest.register_craft({
-        output = 'TransparentNodes:TransparentBlue',
+        output = 'transparent_nodes:transparent_blue',
         recipe = {
                 {'default:glass', 'moreores:mithril_block'},
                 {'moreores:mithril_block', 'default:glass'},
         }
 })
 
-minetest.register_node("TransparentNodes:TransparentBlue", {
+minetest.register_node("transparent_nodes:transparent_blue", {
         description = "Transparent Blue",
         tiles ={"blue_gradient.png"},
         groups = {cracky=3},
-        drop = 'TransparentNodes:TransparentBlue',
+        drop = 'transparent_nodes:transparent_blue',
         legacy_mineral = true,
         sounds = default.node_sound_stone_defaults(),
 })
